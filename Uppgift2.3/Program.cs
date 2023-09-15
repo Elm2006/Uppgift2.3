dfe36236b05002e7f284a5abdb6bcb0dda77ca4b
isel06002 <isel06002@E-PW0234HX>
2023-09-15 12:43:49 +0200

    Add project files.

---
+++ Uppgift2.3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Uppgift_2.__3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många dagar vill du hyra bilen?..");
+            int dagar = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hur många kilometer ska du köra?..");
+            int km = int.Parse(Console.ReadLine());
+            int ans = 300 + km + 500 * dagar;
+            Console.WriteLine("Detta kommer att kosta dig " + ans + " kr");
+        }
+    }
+}
